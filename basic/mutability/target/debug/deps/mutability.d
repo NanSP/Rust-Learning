29164bba8d749d378a480113bbd4caa42d697cb5
Hernandes S. Pereira <hpereira23@outlook.com>
2026-07-22 17:44:23 -0300

    feat: mutability example

---
+++ basic/mutability/target/debug/deps/mutability.d
@@ -0,0 +1,5 @@
+C:\Users\hpere\Desktop\Dev\Rust-Learning\basic\mutability\target\debug\deps\mutability.d: src\main.rs
+
+C:\Users\hpere\Desktop\Dev\Rust-Learning\basic\mutability\target\debug\deps\mutability.exe: src\main.rs
+
+src\main.rs:
---
+++ basic/mutability/target/debug/mutability.d
@@ -0,0 +1,1 @@
+C:\Users\hpere\Desktop\Dev\Rust-Learning\basic\mutability\target\debug\mutability.exe: C:\Users\hpere\Desktop\Dev\Rust-Learning\basic\mutability\src\main.rs
