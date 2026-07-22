79d65daed4d7e6ddf15db0fe8dd8b49c7ce6ed8d
Hernandes S. Pereira <hpereira23@outlook.com>
2026-07-22 17:03:09 -0300

    update: build hello_cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-e713086b6f3e26df.d
@@ -0,0 +1,5 @@
+C:\Users\hpere\Desktop\Dev\Rust-Learning\hello_cargo\target\debug\deps\hello_cargo-e713086b6f3e26df.d: src\main.rs
+
+C:\Users\hpere\Desktop\Dev\Rust-Learning\hello_cargo\target\debug\deps\libhello_cargo-e713086b6f3e26df.rmeta: src\main.rs
+
+src\main.rs:
