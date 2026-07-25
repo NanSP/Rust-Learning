faff71c9aa18c67e654b9a191fea2f12df93eb6c
Hernandes S. Pereira <hpereira23@outlook.com>
2026-07-25 17:48:07 -0300

    feat: ownership example

---
+++ target/debug/deps/ownership-f0302bf4d6631ecb.d
@@ -0,0 +1,5 @@
+C:\Users\hpere\Desktop\Dev\Rust-Learning\target\debug\deps\ownership-f0302bf4d6631ecb.d: basic\ownership\src\main.rs
+
+C:\Users\hpere\Desktop\Dev\Rust-Learning\target\debug\deps\libownership-f0302bf4d6631ecb.rmeta: basic\ownership\src\main.rs
+
+basic\ownership\src\main.rs:
