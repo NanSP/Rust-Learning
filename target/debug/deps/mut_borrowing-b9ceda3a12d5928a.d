03cf3d22d4c41513718779942c02ac40e1ebe237
Hernandes S. Pereira <hpereira23@outlook.com>
2026-07-26 17:50:49 -0300

    feat: mutable borrowing example

---
+++ target/debug/deps/mut_borrowing-b9ceda3a12d5928a.d
@@ -0,0 +1,5 @@
+C:\Users\hpere\Desktop\Dev\Rust-Learning\target\debug\deps\mut_borrowing-b9ceda3a12d5928a.d: basic\mut_borrowing\src\main.rs
+
+C:\Users\hpere\Desktop\Dev\Rust-Learning\target\debug\deps\libmut_borrowing-b9ceda3a12d5928a.rmeta: basic\mut_borrowing\src\main.rs
+
+basic\mut_borrowing\src\main.rs:
