71358ac2b30321275c692432c561667ed9b29b44
Hernandes S. Pereira <hpereira23@outlook.com>
2026-07-29 16:20:55 -0300

    feat: methods example

---
+++ target/debug/deps/methods-786993972d2f8064.d
@@ -0,0 +1,5 @@
+C:\Users\hpere\Desktop\Dev\Rust-Learning\target\debug\deps\methods-786993972d2f8064.d: basic\methods\src\main.rs
+
+C:\Users\hpere\Desktop\Dev\Rust-Learning\target\debug\deps\libmethods-786993972d2f8064.rmeta: basic\methods\src\main.rs
+
+basic\methods\src\main.rs:
