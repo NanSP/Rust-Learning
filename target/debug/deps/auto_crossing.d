d9ed897ea943ec35f6d95df2e00ae8155b1acb00
Hernandes S. Pereira <hpereira23@outlook.com>
2026-07-30 16:23:14 -0300

    refactor: implements structs to auto_crossing course project

---
+++ target/debug/deps/auto_crossing.d
@@ -0,0 +1,5 @@
+C:\Users\hpere\Desktop\Dev\Rust-Learning\target\debug\deps\auto_crossing.d: auto_crossing\src\main.rs
+
+C:\Users\hpere\Desktop\Dev\Rust-Learning\target\debug\deps\auto_crossing.exe: auto_crossing\src\main.rs
+
+auto_crossing\src\main.rs:
