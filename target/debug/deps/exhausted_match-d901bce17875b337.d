11b11e649e7bc63a865cb8f35fe8edbca53b51a2
Hernandes S. Pereira <hpereira23@outlook.com>
2026-08-04 20:12:34 -0300

    feat: add exhausted match example

---
+++ target/debug/deps/exhausted_match-d901bce17875b337.d
@@ -0,0 +1,5 @@
+C:\Users\hpere\Desktop\Dev\Rust-Learning\target\debug\deps\exhausted_match-d901bce17875b337.d: basic\exhausted_match\src\main.rs
+
+C:\Users\hpere\Desktop\Dev\Rust-Learning\target\debug\deps\libexhausted_match-d901bce17875b337.rmeta: basic\exhausted_match\src\main.rs
+
+basic\exhausted_match\src\main.rs:
---
+++ target/debug/deps/exhausted_match.d
@@ -0,0 +1,5 @@
+C:\Users\hpere\Desktop\Dev\Rust-Learning\target\debug\deps\exhausted_match.d: basic\exhausted_match\src\main.rs
+
+C:\Users\hpere\Desktop\Dev\Rust-Learning\target\debug\deps\exhausted_match.exe: basic\exhausted_match\src\main.rs
+
+basic\exhausted_match\src\main.rs:
