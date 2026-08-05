8256dc3070054079e342d400a7084eb39c73e963
Hernandes S. Pereira <hpereira23@outlook.com>
2026-08-05 09:38:21 -0300

    feat: add options example

---
+++ target/debug/deps/options-05c196b1fe4a0295.d
@@ -0,0 +1,5 @@
+C:\Users\hpere\Desktop\Dev\Rust-Learning\target\debug\deps\options-05c196b1fe4a0295.d: basic\options\src\main.rs
+
+C:\Users\hpere\Desktop\Dev\Rust-Learning\target\debug\deps\liboptions-05c196b1fe4a0295.rmeta: basic\options\src\main.rs
+
+basic\options\src\main.rs:
